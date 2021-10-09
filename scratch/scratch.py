bdea58d105b01d515ab65385422b4722e8d64dba
Shawn Yu <shawn.yu5@hotmail.com>
2021-10-08 21:18:20 -0400

    scratch stuff

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,2 +1,7 @@
-print("hello", end="")
-print("world")
+# for i in range(1, 10):
+    # print(i)
+
+number = 10
+for i in range(1, number):
+    print(number)
+# for every number between 1 and 10, we assign it to i, and we print it(i)
