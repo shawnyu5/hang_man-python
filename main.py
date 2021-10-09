85d513a9d5529a335b8fba021d7723dee3ff07ec
Shawn Yu <shawn.yu5@hotmail.com>
2021-10-08 20:21:59 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+class HangMan:
+    # sets the correct word for the game
+    def __init__(self, some_thing):
+        self.correct_word = some_thing
+
+    # gets the number of guesses allowed
+    def getGuesses(self):
+        self.amount_of_guesses = int(input("Number of guesses: "))
+
+    # sets the number of hints allowed
+    def getHints(self):
+        valid = False
+        while valid == False:
+          self.amount_of_hints = int(input("Number of hints: "))
+          if self.amount_of_hints < self.amount_of_guesses:
+            valid = True
+            return
+          print("Too many hints!!!")
+
+    def play(self):
+      self.getGuesses()
+      self.getHints()
+      self.display()
+
+    def display(self):
+      # number of attempts left
+      # the hang man letter squence
+      # apple
+      # _ _ _ _ _
+      print("the correct word:", self.correct_word)
+      print("number of guesses:", self.amount_of_guesses)
+      print("Number of hints", self.amount_of_hints)
+
+
+def main():
+    hangMan = HangMan("apple")
+    hangMan.play()
+
+
+if __name__ == "__main__":
+    main()
